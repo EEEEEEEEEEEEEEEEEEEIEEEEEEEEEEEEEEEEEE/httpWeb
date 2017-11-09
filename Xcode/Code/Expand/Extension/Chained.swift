f4de8c31cdc82721e6678508e0282ea3ce9f892d
Hanxun <wuqiushan3@163.com>
2017-11-09 20:21:54 +0800

    1.增加闭包和链式编程

--- Xcode/Code/Expand/Extension/Chained.swift
+++ Xcode/Code/Expand/Extension/Chained.swift
@@ -9,5 +9,31 @@
 import UIKit
 
 class Chained: NSObject {
-
+    
+    fileprivate var successClosure: (() -> Void)?
+    fileprivate var errorClosure: ((_ err: NSError) -> Void)?
+    
+    func success(_ closure: @escaping () -> Void) -> Chained {
+        successClosure = closure
+        return self
+    }
+    
+    func error(_ closure: @escaping (_ err: NSError) -> Void) -> Chained {
+        errorClosure = closure
+        return self
+    }
+    
+    func request(url: String) -> Chained {
+        print("开始用 \(url) 请求了")
+        ///在回调中，或者 闭包中实现逻辑，略
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.seconds(2), execute: {
+            self.successClosure!()
+        })
+        
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.seconds(4), execute: {
+            self.errorClosure!(NSError(domain: "我在测试失败", code: 321, userInfo:  nil))
+        })
+        
+        return self
+    }
 }
